91120edfac761fa539b7c4a8949486ba5b1a88f4
Dieter Bergmann <dieterbergmann@arcor.de>
2022-03-04 06:18:41 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dieter Bergmann on 04.03.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
